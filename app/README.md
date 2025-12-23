e45d5952d14c6bc59eb1c1fedc94c2bcf23d52b7
devops4sure <devops4sure@gmail.com>
2025-12-23 22:39:26 +0530

    Saving my files after modifying the terrafom configuration

---
+++ app/README.md
@@ -0,0 +1,4 @@
+# React Application – DevOps EKS Project
+
+This is a simple React application used in an end-to-end DevOps project.
+The app will be containerized, pushed to Amazon ECR, and deployed on Amazon EKS.
---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>🚀 DevOps EKS Portfolio Project</h1>
+      <p>React application deployed using CI/CD</p>
+      <p>Docker • Terraform • EKS • GitHub Actions</p>
+    </div>
+  );
+}
+
+export default App;
