5d0ba3c78a1eae1cd654348c65d46961a8a1f6a0
devops4sure <devops4sure@gmail.com>
2025-12-24 19:12:52 +0530

    Fix backend bucket name to match bootstrap

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-eks-terraform-state"
-    key            = "eks/terraform.tfstate"
+    bucket         = "surjeth-devops-eks-tf-state-12345"
+    key            = "devops-eks-project/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
